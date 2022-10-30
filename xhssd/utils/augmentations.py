d709a65127bd82e2fd3aefcfbe435b68d3e59768
H-xianghao-exe <xianghao1103@gmail.com>
2022-10-30 23:09:34 +0800

    update

--- xhssd/utils/augmentations.py
+++ xhssd/utils/augmentations.py
@@ -16,17 +16,13 @@ class Compose(object):
         self.transforms = transforms
 
     def __call__(self, img, boxes=None, labels=None):
+        # print(img, boxes, labels)
         for t in self.transforms:
-            img, boxes, labels = t(img, boxes, labels)
+            print(t)
+            tmp = t(img, boxes, labels)
+            print(len(tmp))
+            img, boxes, labels = tmp
         return img, boxes, labels
-
-
-# ---------------------------------------- 针对像素的数据增强: Begin ----------------------------------------
-class ConvertFromInts(object):
-    """convert image to float32 from int"""
-
-    def __call__(self, image, boxes=None, labels=None):
-        return image.astype(np.float32), boxes, labels
 
 
 class ToAbsoluteCoords(object):
@@ -36,6 +32,30 @@ class ToAbsoluteCoords(object):
         boxes[:, 2] *= width
         boxes[:, 1] *= height
         boxes[:, 3] *= height
+        return image, boxes, labels
+
+
+class ToPersentCoords(object):
+    def __call__(self, image, boxes=None, labels=None):
+        """
+        Args:
+            boxes(ndarray): (nums, 4), 4 means [xmin, ymin, xmax, ymax]
+        """
+        height, width, channels = image.shape
+        boxes[:, 0] /= width
+        boxes[:, 1] /= height
+        boxes[:, 2] /= width
+        boxes[:, 3] /= height
+        return image, boxes, labels
+
+# ---------------------------------------- 针对像素的数据增强: Begin ----------------------------------------
+
+
+class ConvertFromInts(object):
+    """convert image to float32 from int"""
+
+    def __call__(self, image, boxes=None, labels=None):
+        return image.astype(np.float32), boxes, labels
 
 
 class RandomContrast(object):
@@ -100,7 +120,7 @@ class RandomHue(object):
 
 
 class RandomBrightness(object):
-    """针对像素的基于RGB空间的数据增强：随机亮度————将像素值加上/减去同一个值"""
+    """针对像素的基于RGB空间的数据增强: 随机亮度————将像素值加上/减去同一个值"""
 
     def __init__(self, delta=32):
         self.delta = delta
@@ -147,7 +167,8 @@ class PhotometricDistort(object):
                    ConvertColor(transform='HSV'),  # 色彩空间转换 BGR->HSV
                    RandomSaturation(),  # 随机饱和度
                    RandomHue(),  # 随机色调
-                   ConvertColor(current='HSV', transform='BGR'),  # 转换色彩空间 HSV -> BGR
+                   # 转换色彩空间 HSV -> BGR
+                   ConvertColor(current='HSV', transform='BGR'),
                    RandomContrast()  # 随机对比度
                    ]
         self.rand_brightness = RandomBrightness()  # 随机亮度
@@ -202,12 +223,16 @@ class Expand(object):
             # 获取图像的各个维度
             height, width, depth = image.shape
             # 随机缩放尺度
-            factor = np.random.uniform(1, 4)  # expand后的图片大小是(height*factor, width*factor, depth)
+            # expand后的图片大小是(height*factor, width*factor, depth)
+            factor = np.random.uniform(1, 4)
             # 注意：图像原点在左上角
-            left = np.random.uniform(0, width * factor - width)  # 原图最左边的位置在放大后图像中的坐标
-            top = np.random.uniform(0, height * factor - height)  # 原图最顶部的位置在放大后图像中的坐标
+            left = np.random.uniform(
+                0, width * factor - width)  # 原图最左边的位置在放大后图像中的坐标
+            top = np.random.uniform(
+                0, height * factor - height)  # 原图最顶部的位置在放大后图像中的坐标
             # 扩大后的图片维度
-            expand_image = np.zeros((int(height * factor), int(width * factor), depth), dtype=image.dtype)
+            expand_image = np.zeros(
+                (int(height * factor), int(width * factor), depth), dtype=image.dtype)
             expand_image[:, :, :] = self.mean
             expand_image[int(top): int(top + height), int(left):int(left + width)] = image
 
@@ -246,6 +271,7 @@ def iou(boxes_a, box_b):
 
     union = area_a + area_b - inter
     return inter/union
+
 
 class RandomSampleCrop(object):
     """随机裁剪：随机裁掉原图中的一部分， 然后检查边界框或者目标整体是否被裁掉，如果目标整体被裁掉，则舍弃这次随机过程"""
@@ -290,7 +316,8 @@ class RandomSampleCrop(object):
                 top = np.random.uniform(0, height - h)
 
                 # 由此确定图像所在坐标
-                rect = np.array([int(left), int(top), int(left + w), int(top + h)])
+                rect = np.array(
+                    [int(left), int(top), int(left + w), int(top + h)])
                 # 求解原始的boxes和新的图像的IoU
                 overlap = iou(boxes, rect)
 
@@ -309,12 +336,43 @@ class RandomSampleCrop(object):
                 current_boxes = boxes[mask, :].copy()
                 current_labels = labels[mask]
                 # 对于中心点在裁剪后图像区域的box的边界重新设定，原边界有可能超出了裁剪后图像区域
-                current_boxes[:, :2] = np.maximum(current_boxes[:, :2], rect[:2]) - rect[:2]
-                current_boxes[:, 2:] = np.minimum(current_boxes[:, 2:], rect[2:]) - rect[:2]
+                current_boxes[:, :2] = np.maximum(
+                    current_boxes[:, :2], rect[:2]) - rect[:2]
+                current_boxes[:, 2:] = np.minimum(
+                    current_boxes[:, 2:], rect[2:]) - rect[:2]
                 return current_img, current_boxes, current_labels
 
 
 # ---------------------------------------- 针对图像的数据增强：End ----------------------------------------
+
+class Resize(object):
+    def __init__(self, size=300) -> None:
+        """Resize image shape to (size, size)
+
+        Args:
+            size (int): image shape after resizing
+        """
+        self.size = size
+
+    def __call__(self, image, boxes=None, labels=None):
+        image = cv2.resize(image, (self.size, self.size))
+        return image, boxes, labels
+
+
+class SubstractMeans(object):
+    def __init__(self, mean) -> None:
+        """
+
+        Args:
+            mean (_type_): _description_
+        """
+        self.mean = np.array(mean, dtype=np.float32)
+
+    def __call__(self, image, boxes=None, labels=None):
+        image = image.astype(np.float32)
+        image -= self.mean
+        return image.astype(np.float32), boxes, labels
+
 
 class SSDAugmentation(object):
     def __init__(self, size=300, mean=(104, 117, 123)):
@@ -322,25 +380,33 @@ class SSDAugmentation(object):
         self.size = size
         self.augment = Compose([
             ConvertFromInts(),
-            ConvertColor(),
-            RandomSaturation(),
+            ToAbsoluteCoords(),
+            PhotometricDistort(),
+            Expand(self.mean),
+            RandomSampleCrop(),
+            RandomMirror(),
+            ToPersentCoords(),
+            Resize(self.size),
+            SubstractMeans(self.mean)
         ])
 
-    def __call__(self, image, boxes=None, labels=None):
+    def __call__(self, image, boxes, labels):
         """
         Args:
             boxes(ndarray): [[xmin, ymin, xmax, ymax], ...]
         """
-        return self.augment(image)
+        return self.augment(image, boxes, labels)
 
 
 if __name__ == '__main__':
     image = np.random.randint(0, 255, (300, 300, 3))
+    boxes = np.array([[45, 56, 223, 256], [23, 78, 234, 234]])
+    labels = np.array([3, 2])
     # from matplotlib import pyplot as plt
     # plt.subplot(121)
     # plt.imshow(image)
     augument = SSDAugmentation()
-    image, boxes, labels = augument(image)
+    image, boxes, labels = augument(image, boxes, labels)
     # plt.subplot(122)
     # plt.imshow(image)
     # plt.show()
