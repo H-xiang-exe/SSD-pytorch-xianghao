c0eff409faf9750f5d892a84d48c1313ee168c90
H-xiang-exe <xianghao1103@gmail.com>
2022-11-17 10:02:56 +0800

    style(parse.py): improve readability

--- config/parse.py
+++ config/parse.py
@@ -10,24 +10,25 @@ def str2bool(v):
 parser = argparse.ArgumentParser(
     description="Single Shot MultiBox Detector Training With Pytorch")
 
-parser.add_argument("--dataset", default='VOC2007',
-                    choices=["VOC2007", "VOC2012", "COCO"], type=str, help="VOC or COCO")
+# Data
+data_arg = parser.add_argument_group('Data')
+data_arg.add_argument("--dataset", default='VOC2007', choices=["VOC2007", "VOC2012", "COCO"], type=str,
+                      help="VOC or COCO")
+data_arg.add_argument('--batch_size', default=8, type=int, help='batch size of train/test')
+data_arg.add_argument('--test_batch_size', default=8, type=int, help='batch size of train/test')
+data_arg.add_argument('--num_workers', default=1, type=int)
+data_arg.add_argument('--pin_memory', default=False, type=bool)
 
-parser.add_argument('--batch-size', default=8, type=int, help='batch size of train/test')
+# solver
+solver_arg = parser.add_argument_group('Solver')
+solver_arg.add_argument("--lr", default=1e-3, type=float, help="initial learning rate")
+solver_arg.add_argument("--momentum", default=0.9, type=float, help="Momentum value for optim")
+solver_arg.add_argument("--weight_decay", default=5e-4, type=float, help="weight decay for SGD")
 
-parser.add_argument("--lr", default=1e-3, type=float,
-                    help="initial learning rate")
-parser.add_argument("--momentum", default=0.9, type=float,
-                    help="Momentum value for optim")
-parser.add_argument("--weight_decay", default=5e-4,
-                    type=float, help="weight decay for SGD")
-
-parser.add_argument('--num_workers', default=1, type=int)
-
-parser.add_argument("--cuda", default=True, type=str2bool,
-                    help="Use CUDA to train modeling")
-parser.add_argument("--visdom", default=False, type=str2bool,
-                    help="Use Visdom for loss visualization")
+# Misc
+misc_arg = parser.add_argument_group('Misc')
+misc_arg.add_argument("--cuda", default=True, type=str2bool, help="Use CUDA to train modeling")
+misc_arg.add_argument("--visdom", default=False, type=str2bool, help="Use Visdom for loss visualization")
 
 
 def get_config():
