73c78a4e90af34b5689fcbfac3f20f596cd72ca2
H-xiang-exe <xianghao1103@gmail.com>
2022-11-29 21:17:05 +0800

    update

--- utils/logger.py
+++ utils/logger.py
@@ -3,5 +3,6 @@ import logging
 def setup_logger(name, distributed_rank, save_dir=None):
     # 创建logger记录器
     logger = logging.getLogger()
+    # 设置日志记录级别
     logger.setLevel(logging.DEBUG)
     
