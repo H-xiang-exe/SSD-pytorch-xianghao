fe8b914421c37154dfda685e290da728a0f077d1
H-xiang-exe <xianghao1103@gmail.com>
2022-12-05 20:14:42 +0800

    update

--- modeling/post_processor.py
+++ modeling/post_processor.py
@@ -3,5 +3,16 @@ class PostProcessor(object):
         super(PostProcessor, self).__init__()
         self.cfg = cfg
 
-    def __call__(self):
-        pass
+    def __call__(self, detections):
+        # 模型输出
+        batch_scores, batch_boxes = detections
+        device = batch_scores.device
+        batch_size = batch_scores.shape[0]
+
+        results = []
+        for scores, boxes in zip(batch_scores, batch_boxes):
+            num_boxes = scores.shape[0]  # scores: (num_boxes, 4)
+            num_classes = boxes.shape[0]  # boxes: (num_boxes, cls)
+
+            boxes = boxes.view(num_boxes, 1, 4).expand(num_boxes, num_classes, 4)
+            print(boxes)
