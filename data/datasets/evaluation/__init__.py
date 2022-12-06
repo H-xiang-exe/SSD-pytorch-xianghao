38c799bae7b194f895c1a35fbb45a87687695786
H-xiang-exe <xianghao1103@gmail.com>
2022-12-06 16:00:12 +0800

    update

--- data/datasets/evaluation/__init__.py
+++ data/datasets/evaluation/__init__.py
@@ -1,3 +1,10 @@
-
+from data.datasets import VOCDataset, COCODataset
 def evaluate(dataset, predictions, *args, **kwargs):
-    pass
+    """evaluate dataset using different model based on dataset type.
+
+    Args:
+        dataset (Dataset): Dataset object
+        predictions (list[image_id: container(boxes, labels, scores)]): Each item in the list represents the
+            prediction results for one image.
+    """
+    
