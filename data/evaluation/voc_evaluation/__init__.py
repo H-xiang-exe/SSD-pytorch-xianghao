3b2db8541a1995dc9abad91ce6e98353d920ab3e
H-xiang-exe <xianghao1103@gmail.com>
2022-12-10 17:01:21 +0800

    push

--- data/evaluation/voc_evaluation/__init__.py
+++ data/evaluation/voc_evaluation/__init__.py
@@ -6,3 +6,17 @@ def voc_evaluation(dataset, predictions,):
         predictions (list[image_id: container(boxes, labels, scores)]): Each item in the list represents the
             prediction results for one image.
     """
+    # 数据集类别
+    class_names = dataset.class_names
+
+    pred_boxes_list = []
+    pred_labels_list = []
+    pred_scores_list = []
+
+    gt_boxes_list = []
+    gt_labels_list = []
+    gt_scores_list = []
+
+    # 逐一考虑数据集中的每张图片
+    for i in range(len(dataset)):
+        pass
